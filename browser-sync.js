20e0774caedc687d679f57f6dd68f18c8de2c998
Olesia Shcherbenko <oshch96@gmail.com>
2024-07-15 18:45:03 +0200

    :tada: first commit

---
+++ browser-sync.js
@@ -0,0 +1,33 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "ui": {
+        "port": 3001
+    },
+    "watchEvents": [
+        "change",
+        "add",
+        "unlink",
+        "addDir",
+        "unlinkDir"
+    ],
+    "watch": true,
+    "ignore": ['node_modules', '.cache', 'scss', 'package.json', 'package-lock.json', '*/**.map'],
+    "server": "dist",
+    "files": "dist",
+    "port": 3000,
+    "codeSync": true,
+    "notify": true
+};
